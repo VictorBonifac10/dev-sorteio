606084d6984a5626764014acc8cbec96f514681a
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-05-31 15:16:00 -0300

    Resolução de bugs e finalização do script

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-
 const sortear = document.querySelector(".btn")
 
 function sortearNum() {
     const numero1 = parseInt(document.querySelector("#num1").value);  // Isso transforma o texto "5" no número 5
     const numero2 = parseInt(document.querySelector("#num2").value);
-
-    //console.log(numero1)
-    //console.log(numero2)
+    const numSorteado = document.querySelector(".numberSorteado");
+    const aviso = document.querySelector(".msg-aviso")
 
     const randomNumber = Math.floor(Math.random() * (numero2 - numero1 + 1)) + numero1;
     console.log(randomNumber)
+
+    numSorteado.innerHTML = "Nº" + randomNumber
+
+if(isNaN(numero1) || isNaN(numero2)) { // verifica se a variel é NaN, caso for a seguinte msg abaixo aparece
+        aviso.innerHTML = "É preciso selecionar um intervalo de números para o sorteio"
+    }else{
+        console.log("tudo certo seu numero é " + randomNumber)
+        aviso.innerHTML = ""
+    }
 }
 
 sortear.addEventListener("click", sortearNum)
